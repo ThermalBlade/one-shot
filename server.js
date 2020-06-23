070b8e41d26fb4680d3bc2010d36d7d3d61a0de9
ThermalBlade <copr5155@colorado.edu>
2020-06-23 12:51:01 -0700

    Testing for EB

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 
 app.get('/flower', (req, res) => {
     res.json({
-        name: 'This is the landing page for One Shot',
+        name: 'This is still the landing page im just testing things.',
         color: 'Working title, I have no fucking idea what to call this still.'
     })
 })
