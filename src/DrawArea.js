b8495d8b730edf18bbc7a99e638ebe55fac8f7a0
dado3899 <dado3899@colorado.edu>
2020-07-23 16:39:53 -0600

    Draw on screen Feature
    We can now draw on the screen and create boxes. not yet hover over but in progress

--- src/DrawArea.js
+++ src/DrawArea.js
@@ -1,9 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import { Button, Box } from '@material-ui/core';
  
 function DrawArea() {
     const [lines, setlines] = useState([])
     const [isDrawing, setisDrawing] = useState(false)
+    const [ShowArea, setShowArea] = useState(false)
+    const [Maxx, setMaxx] = useState(0);
+    const [Minx, setMinx] = useState(100000);
+    const [Maxy, setMaxy] = useState(0);
+    const [Miny, setMiny] = useState(100000);
+
+    const [mystyle, setmystyle] = useState({
+        position: 'absolute'
+    })
+
+    function Finish() {
+        setShowArea(false);
+        setlines([])
+        setisDrawing(false)
+        setmystyle({
+            width: Maxx - Minx + "px",
+            height: Maxy - Miny + 'px',
+            marginLeft: Minx + 'px',
+            marginTop: Miny + 'px',
+            background: 'red',
+            position: 'absolute'
+        })
+        setMaxx(0)
+        setMinx(100000)
+        setMaxy(0)
+        setMiny(100000)
+    }
+
+    function Start() {
+        setShowArea(true)
+    }
 
     function handleMouseDown(mouseEvent) {
         if (mouseEvent.button != 0) {
@@ -11,12 +43,9 @@ function DrawArea() {
         }
 
         const point = relativeCoordinatesForEvent(mouseEvent);
-        /*
-        this.setState(prevState => ({
-            lines: prevState.lines.push(new Immutable.List([point])),
-            isDrawing: true
-        }));*/
-        setlines(lines => lines.concat([point]))
+        let Temp = [...lines]
+        Temp.push([point])
+        setlines(Temp)
         setisDrawing(true)
     }
 
@@ -26,11 +55,9 @@ function DrawArea() {
         }
 
         const point = relativeCoordinatesForEvent(mouseEvent);
-        /*
-        this.setState(prevState => ({
-            lines: prevState.lines.updateIn([prevState.lines.size - 1], line => line.push(point))
-        }));*/
-        setlines(lines => lines.concat([point]))
+        let Temp = [...lines]
+        Temp[Temp.length-1].push(point)
+        setlines(Temp)
     }
 
     function handleMouseUp() {
@@ -38,44 +65,62 @@ function DrawArea() {
     }
 
     function relativeCoordinatesForEvent(mouseEvent) {
-        const boundingRect = this.refs.drawArea.getBoundingClientRect();
-        /*
-        return new Immutable.Map({
+        let boundingRect = document.getElementById('drawArea').getBoundingClientRect();
+        //console.log(mouseEvent.clientX, Maxx)
+        if (isDrawing) {
+            if (mouseEvent.clientX > Maxx) {
+                setMaxx(mouseEvent.clientX)
+            }
+            else if (mouseEvent.clientX < Minx) {
+                setMinx(mouseEvent.clientX)
+            }
+            if (mouseEvent.clientY > Maxy) {
+                setMaxy(mouseEvent.clientY)
+            }
+            else if (mouseEvent.clientY < Miny) {
+                setMiny(mouseEvent.clientY)
+            }
+        }
+        return {
             x: mouseEvent.clientX - boundingRect.left,
             y: mouseEvent.clientY - boundingRect.top,
-        });*/
+        }
+    }
+
+    function Drawing({ lines }) {
+        //console.log(lines)
+        return (
+            <svg className="drawing">
+                {lines.map((line, index) => (
+                    <DrawingLine key={index} line={line} />
+                ))}
+            </svg>
+        );
+    }
+
+    function DrawingLine({ line }) {
+        let pathData = "M " + line.map(p => p.x + ' ' + p.y).join(" L ");
+        return <path className="path" d={pathData} />;
     }
     return (
-        <div
-            className="drawArea"
-            ref="drawArea"
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-        >
-            <Drawing lines={this.state.lines} />
+        <div>
+            {ShowArea
+                ? <div
+                    id='drawArea'
+                    className="drawArea"
+                    onMouseDown={handleMouseDown}
+                    onMouseMove={handleMouseMove}
+                    onMouseUp={handleMouseUp}
+                >
+                    <Drawing lines={lines} />
+                    <Button variant="outlined" onClick={() => Finish()}> Finish </Button>
+                </div>
+                : <Button variant="outlined" onClick={() => Start()}> Start Draw </Button>
+            }
+            <h1 style={mystyle}></h1>
         </div>
     );
 }
 
-function Drawing({ lines }) {
-    return (
-        <svg className="drawing">
-            {lines.map((line, index) => (
-                <DrawingLine key={index} line={line} />
-            ))}
-        </svg>
-    );
-}
 
-function DrawingLine({ line }) {
-    const pathData = "M " +
-        line
-            .map(p => {
-                return `${p.get('x')} ${p.get('y')}`;
-            })
-            .join(" L ");
-
-    return <path className="path" d={pathData} />;
-}
-
-export default Map;
+export default DrawArea;
