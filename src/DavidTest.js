dae5bd91e4a22b311fd28620881272888c3bf27e
dado3899 <dado3899@colorado.edu>
2020-07-29 16:27:16 -0600

    Draw + popups
    You can now draw on the screen, type in your hover text and it will do it, some reason the hover text created shows up in the middle though

--- src/DavidTest.js
+++ src/DavidTest.js
@@ -17,12 +17,12 @@ function App() {
         <div className="App">
             <DevDraw />
             <h1><Popup Title={'Thomas'} Content={[<div className="Popup"> <h1>Is a Tall boi</h1> <h1>Should stop stabbing people</h1> <h1>RIP Pig</h1> <img src={Memes} alt="Memes" /> </div>]} /> </h1>
-            <Hover Holder={[<RandButton Number={20} Bonus={1} Words={'Dexterity'} />]} Content={[<div className="Popup"> <h1>Dexterity is the stat for all menuevers that require percision and accuracy. Roll this for all of you rolling needs blah blah blah blah blah</h1> </div>]} />
+            <p className='buttons'><Hover Holder={[<RandButton Number={20} Bonus={1} Words={'Dexterity'} />]} Content={[<div className="Popup"> <h1>Dexterity is the stat for all menuevers that require percision and accuracy. Roll this for all of you rolling needs blah blah blah blah blah</h1> </div>]} /> </p>
             <RandButton Number={20} Bonus={1} Words={'Strength'} />
             <MultiButton />
             <p>It's <time dateTime={responce}>{responce}</time></p>
-            <p><Hover Holder={'Townhall'} Content={[<div className="Popup"> <h1>This is the hall for the town</h1> </div>]} /></p>
-            <Hover Holder={[<MultiButton />]} Content={[<div className="Popup"> <h1>To just roll a number with no bonuses</h1> </div>]} />
+            <p className='buttons'><Hover Holder={'Townhall'} Content={[<div className="Popup"> <h1>This is the hall for the town</h1> </div>]} /></p>
+            <p className='buttons'><Hover Holder={[<MultiButton />]} Content={[<div className="Popup"> <h1>To just roll a number with no bonuses</h1> </div>]} /></p>
         </div>
     );
 }
--- src/DrawArea.js
+++ src/DrawArea.js
@@ -1,37 +1,69 @@
 import React, { useState, useRef } from 'react';
 import './App.css';
-import { Button, Box } from '@material-ui/core';
+import { Button, Box, TextField } from '@material-ui/core';
+import Hover from './Hoverpopup'
  
 function DrawArea() {
     const [lines, setlines] = useState([])
     const [isDrawing, setisDrawing] = useState(false)
     const [ShowArea, setShowArea] = useState(false)
+    const [inputText, setinputText] = useState('')
     const [Maxx, setMaxx] = useState(0);
     const [Minx, setMinx] = useState(100000);
     const [Maxy, setMaxy] = useState(0);
     const [Miny, setMiny] = useState(100000);
 
-    const [mystyle, setmystyle] = useState({
-        position: 'absolute'
-    })
+    const [mystyle, setmystyle] = useState([{
+        width: "100px",
+        height: '100px',
+        marginLeft: '10px',
+        marginTop: '20px',
+        background: 'red',
+        position: 'absolute',
+        opacity: '0.5'
+    },
+    {
+        width: "100px",
+        height: '100px',
+        marginLeft: '10px',
+        marginTop: '130px',
+        background: 'red',
+        position: 'absolute',
+        opacity: '0.5'
+    }
+    ])
+
+    const [desc, setdesc] = useState(["Desc 1", "desc 2"])
 
     function Finish() {
+        let boundingRect = document.getElementById('drawArea').getBoundingClientRect();
         setShowArea(false);
         setlines([])
         setisDrawing(false)
-        setmystyle({
+        let Temp = [...mystyle]
+        Temp.push({
             width: Maxx - Minx + "px",
             height: Maxy - Miny + 'px',
             marginLeft: Minx + 'px',
-            marginTop: Miny + 'px',
+            marginTop: Miny - boundingRect.top + 'px',
             background: 'red',
-            position: 'absolute'
+            position: 'absolute',
+            opacity: '0.5'
         })
+        setmystyle(Temp)
+        let TempText = [...desc]
+        TempText.push(inputText)
+        setdesc(TempText)
+        setinputText('')
         setMaxx(0)
         setMinx(100000)
         setMaxy(0)
         setMiny(100000)
     }
+
+    const handleChange = (event) => {
+        setinputText(event.target.value)
+    };
 
     function Start() {
         setShowArea(true)
@@ -104,8 +136,9 @@ function DrawArea() {
     }
     return (
         <div>
-            {ShowArea
-                ? <div
+            <Button variant="outlined" onClick={() => Start()}> Start Draw </Button>
+            {ShowArea &&
+                <div
                     id='drawArea'
                     className="drawArea"
                     onMouseDown={handleMouseDown}
@@ -113,11 +146,23 @@ function DrawArea() {
                     onMouseUp={handleMouseUp}
                 >
                     <Drawing lines={lines} />
+                    <p align="center">
+                        <label className="FormField__Label">Text: </label>
+                        <TextField
+                            value={inputText}
+                            onChange={handleChange}
+                        >
+                        </TextField></p>
                     <Button variant="outlined" onClick={() => Finish()}> Finish </Button>
+
                 </div>
-                : <Button variant="outlined" onClick={() => Start()}> Start Draw </Button>
             }
-            <h1 style={mystyle}></h1>
+            {mystyle.map((Boxes, index) => (
+                <Hover Holder={<h1 style={Boxes}></h1>} Content={[<div className="Popup"> <h1>{desc[index]}</h1></div>]} />
+            ))}
+
+            
+            
         </div>
     );
 }
